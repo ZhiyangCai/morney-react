c3eb9dda41fb4553f48fa2bf05497f7418422c49
zhiyangcai <2313120974@qq.com>
2020-07-10 09:22:23 +0800

    添加svg文件

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,70 @@
-import React from 'react';
+import React from "react";
+import {
+    HashRouter as Router,
+    Switch,
+    Route,
+    Link,
+    Redirect
+} from "react-router-dom";
+import styled from 'styled-components';
+import Nav from './components/Nav';
+
+const Wrapper=styled.div`
+border:1px solid red;
+min-height:100vh;
+display:flex;
+flex-direction:column;
+`;
+const Main=styled.div`
+border:1px solid green;
+flex-grow:1;
+overflow: auto;
+`;
 
 
+ function App() {
+    return (
+        <Router>
+            <Wrapper>
+                <Main>
+                    <Switch>
+                        <Route path="/tags">
+                            <Tags />
+                        </Route>
+                        <Route path="/money">
+                            <Money />
+                        </Route>
+                        <Route path="/statistics">
+                            <Statistics />
+                        </Route>
+                        <Redirect exact from="/" to="/money"/>
+                        <Route path="*">
+                            <NoMatch/>
+                        </Route>
+                    </Switch>
+                </Main>
+               <Nav/>
 
-function App() {
-  return (
-    <div>
-hello
-    </div>
-  );
+            </Wrapper>
+        </Router>
+    );
+}
+function NoMatch(){
+    return (
+        <div>页面不存在</div>
+    );
+
+}
+function Statistics() {
+    return <h2>统计</h2>;
+}
+
+function Tags() {
+    return <h2>标签</h2>;
+}
+
+function Money() {
+    return <h2>记账</h2>;
 }
 
 export default App;
