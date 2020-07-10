b14ad5586d721c3b2edf70fe8aa4dfd210ac6239
zhiyangcai <2313120974@qq.com>
2020-07-10 23:43:18 +0800

    创建Layout组件

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,14 @@ import {
 } from "react-router-dom";
 import styled from 'styled-components';
 import Nav from './components/Nav';
+import Layout from './components/Layout';
 
-const Wrapper=styled.div`
-border:1px solid red;
-min-height:100vh;
-display:flex;
-flex-direction:column;
-`;
-const Main=styled.div`
-border:1px solid green;
-flex-grow:1;
-overflow: auto;
-`;
 
 
  function App() {
     return (
         <Router>
-            <Wrapper>
-                <Main>
+
                     <Switch>
                         <Route path="/tags">
                             <Tags />
@@ -42,10 +31,7 @@ overflow: auto;
                             <NoMatch/>
                         </Route>
                     </Switch>
-                </Main>
-               <Nav/>
 
-            </Wrapper>
         </Router>
     );
 }
@@ -56,15 +42,28 @@ function NoMatch(){
 
 }
 function Statistics() {
-    return <h2>统计</h2>;
+    return (
+            <Layout>
+                <h2>统计页面</h2>
+            </Layout>
+    );
 }
 
 function Tags() {
-    return <h2>标签</h2>;
+    return (
+        <Layout>
+            <h2>标签</h2>
+        </Layout>
+    );
 }
 
 function Money() {
-    return <h2>记账</h2>;
+
+    return (
+        <Layout>
+            <h2>记账</h2>
+        </Layout>
+    );
 }
 
 export default App;
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 import {Link} from 'react-router-dom';
 import React from 'react';
-import Icon from 'components/icon';
+import Icon from 'components/Icon';
 
 
 const Navwrapper=styled.div`
